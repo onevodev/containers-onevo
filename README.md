75e8dc5b895035d68db167da52ba4fcc2a3d2cce
mtf <m.ferrante@onevo.it>
2022-11-09 16:41:30 +0100

    cleaned up everything (?)

---
+++ README.md
@@ -0,0 +1,2 @@
+
+custom built containers
--- helium-local-system/scripts/install-influxdb.sh
+++ helium-local-system/scripts/install-influxdb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # install influx + telegraf
-
 wget -qO- https://repos.influxdata.com/influxdb.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
 export DISTRIB_ID=$(lsb_release -si); export DISTRIB_CODENAME=$(lsb_release -sc)
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | tee /etc/apt/sources.list.d/influxdb.list > /dev/null
