8eb71741468304124eaa9c8770e90defed2de264
mtf <m.ferrante@onevo.it>
2022-11-16 18:19:36 +0100

    ahhh it committed changing command to old path

--- helium-local-system/build.sh
+++ helium-local-system/build.sh
@@ -9,7 +9,7 @@ podman build -t $IMAGE .
 # setup image
 podman run -it --name $IMAGE localhost/$IMAGE /usr/local/bin/setup.sh
 # commit image
-podman commit -c "CMD=/scripts/start-daemon.sh" $(podman ps -a | grep $IMAGE | awk -F " " '{print$1}') localhost/$IMAGE
+podman commit -c "CMD=start-daemon.sh" $(podman ps -a | grep $IMAGE | awk -F " " '{print$1}') localhost/$IMAGE
 # remove template
 podman rm $(podman ps -a | grep $IMAGE | awk -F " " '{print$1}')
 # done
