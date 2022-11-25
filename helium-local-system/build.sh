10c31b03a5e7e1616f8393d7e8a1add3de2eaff8
mtf <m.ferrante@onevo.it>
2022-11-25 14:38:01 +0100

    fixed a grep

--- helium-local-system/build.sh
+++ helium-local-system/build.sh
@@ -9,8 +9,8 @@ podman build -t $IMAGE .
 # setup image
 podman run -it --name $IMAGE localhost/$IMAGE /usr/local/bin/setup.sh
 # commit image
-podman commit -c "CMD=start-daemon.sh" $(podman ps -a | grep $IMAGE | awk -F " " '{print$1}') localhost/$IMAGE
+podman commit -c "CMD=start-daemon.sh" $(podman ps -a | grep -w $IMAGE | awk -F " " '{print$1}') localhost/$IMAGE
 # remove template
-podman rm $(podman ps -a | grep $IMAGE | awk -F " " '{print$1}')
+podman rm $(podman ps -a | grep -w $IMAGE | awk -F " " '{print$1}')
 # done
 echo "Done."
