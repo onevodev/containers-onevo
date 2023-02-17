20aded51d1adcc0828a67bd78e14a3318b35a905
mtf <m.ferrante@onevo.it>
2023-02-17 13:37:59 +0100

    fixed script that might exit with rc1

--- helium-pod-environment/stop.sh
+++ helium-pod-environment/stop.sh
@@ -8,4 +8,4 @@ source ./.env
 [ -z "$(podman pod ps | grep -w ${POD_NAME} | grep -v CONTAINER)" ] && (echo "Can't find a pod named '${POD_NAME}'!" ; exit 1)
 podman pod stop ${POD_NAME}
 # also remove if specified
-[ "${1}" == "--rm" ] && podman pod rm ${POD_NAME}
+[ "${1}" == "--rm" ] && podman pod rm ${POD_NAME} || exit 0
